5040ebd96eb708624d5e674ec273186662e5ea35
miyay <yuuma_117@hotmail.com>
2014-08-25 00:35:35 +0900

    Userモデルを適切にインポートできるように

--- config/initializers/rails_admin_import.rb
+++ config/initializers/rails_admin_import.rb
@@ -1,11 +1,11 @@
 RailsAdminImport.config do |config|
   config.model User do
     excluded_fields do
-      [:version]
+      [:version, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip]
     end
     label :name
     extra_fields do
-      []
+      [:password, :password_confirmation]
     end
   end
 end
